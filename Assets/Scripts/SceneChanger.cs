8bece91e9b1b01abdb62d1fae172a254f2ce9975
FIN-THE-HUMAN <malyshevi@yandex.ru>
2017-11-12 22:54:53 +0300

    Added test menu

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string Scene;
+
+    public void ChangeScene()
+    {
+        Application.LoadLevel(Scene);
+    }
+}
